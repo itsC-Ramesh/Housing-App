587dd58966597a55472cfbdcd5e235e7aaca9620
Ramesh <itsC.Ramesh@gmail.com>
2022-08-14 12:24:48 +0530

    Added Property Card, Values passed are dummy

--- Housing-App-UI/src/app/app.module.ts
+++ Housing-App-UI/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    PropertyListComponent,
+    PropertyCardComponent,
+    NavBarComponent
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule
